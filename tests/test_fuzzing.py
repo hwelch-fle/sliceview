ee588ca06f5fadc95dd87c9eb6d66db24c3e4000
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-04-15 16:24:03 -0400

    this needs some work, there's gotta be a better way to test this

--- tests/test_fuzzing.py
+++ tests/test_fuzzing.py
@@ -15,10 +15,13 @@ def test_fuzzy(lst: list[int], sl: slice):
     sv = sliceview(lst, sl)
     assert list(sv) == correct_answer(lst, sl), (sv.range, sv.slice, sl)
 
-@given(lst=st.lists(st.integers(), min_size=100, max_size=100), sl=st.slices(100))
+@given(lst=st.lists(st.integers(), min_size=5, max_size=5), sl=st.slices(5))
 def test_slice_assignment(lst: list[int], sl: slice):
-    _lst = lst[sl].copy()
+    _lst = lst[sl]
+    if len(lst) < 2:
+        return
     sv = sliceview(lst, sl)
-    _lst[0:2] = [1,1]
-    sv[0:2] = [1,1]
-    assert sv == _lst
+    
+    assert list(sv) == _lst, ('initial', str(sv), str(_lst), sv.slice)
+    assert list(sv[0:2]) == _lst[0:2], ('sub-slice', str(sv), str(_lst), sv.slice)
+    assert len(_lst) == len(sv), ('length', str(sv), str(_lst), sv.slice)

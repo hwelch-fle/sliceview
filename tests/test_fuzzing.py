a3bc6c95b906e018fc8852a72b7c8946428a6bdf
hwelch-fle <hwelch@finelines-engineering.com>
2026-04-15 00:19:10 -0400

    more fuzzing

--- tests/test_fuzzing.py
+++ tests/test_fuzzing.py
@@ -1,5 +1,4 @@
 from collections.abc import Sequence
-from typing import Any
 
 from hypothesis import (
     given, 
@@ -12,6 +11,14 @@ def correct_answer[T](seq: Sequence[T], sl: slice) -> list[T]:
     return list(seq[sl])
 
 @given(lst=st.lists(st.integers(), min_size=100, max_size=100), sl=st.slices(100))
-def test_fuzzy(lst: list[list[int]], sl: slice):
+def test_fuzzy(lst: list[int], sl: slice):
     sv = sliceview(lst, sl)
     assert list(sv) == correct_answer(lst, sl), (sv.range, sv.slice, sl)
+
+@given(lst=st.lists(st.integers(), min_size=100, max_size=100), sl=st.slices(100))
+def test_slice_assignment(lst: list[int], sl: slice):
+    _lst = lst[sl].copy()
+    sv = sliceview(lst, sl)
+    _lst[0:2] = [1,1]
+    sv[0:2] = [1,1]
+    assert sv == _lst
